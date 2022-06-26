46d160e0991323d15db3c1868ecf984c7b38a821
Madina <m.kuldeeva06@mail.ru>
2022-06-26 15:53:28 +0600

    done task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,14 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  if (arr1.length !== arr2.length) return false;
+    for (var i = 0, len = arr1.length; i < len; i++){
+        if (arr1[i] !== arr2[i]){
+            return false;
+        }
+    }
+    return true; 
+
 
   return result; // boolean
 }
@@ -10,6 +18,7 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  return arr.filter(item => item > 0 && item % 3 == 0).map(item => item *10);
 
   return resultArr; // array
 }
