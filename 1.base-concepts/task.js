f021bdc92c4992040d91a2f09d44dbef59ffa7f1
Madina <m.kuldeeva06@mail.ru>
2022-06-26 18:16:13 +0600

    task done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,22 @@
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  
+  let d = Math.pow(b, 2) - (4 * a * c);
+  let x1 = (-b + Math.sqrt(d)) / (2 * a);
+  let x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+  if (d < 0) {
+    arr = [];
+  } else if (d == 0) {
+    let x = -b / (2 * a)
+    arr = [x];
+  } else {
+    
+    arr = [x1, x2];
+  }
+
+  
+
   return arr; // array
 }
