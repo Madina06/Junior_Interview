ff0c3399f9000dc42c6845446b10e60033d030f9
MadinaKuldeeva <170107147@stu.sdu.edu.kz>
2022-06-27 11:42:07 +0600

    task done

--- promises/task.js
+++ promises/task.js
@@ -1,2 +1,4 @@
-const result = promiseFunction()
-console.log(`Промис ${typeof result === 'string' ? '' : 'НЕ'} работает ${typeof result === 'string' ? result : ''}`)
+promiseFunction().then((result) => {
+	console.log(`Промис ${typeof result === 'string' ? '' : 'НЕ'} работает ${typeof result === 'string' ? result : ''}`)	
+})
+
